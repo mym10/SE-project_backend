342bd3ad2436595976a79fc6463e9b2fd8d6fdae
Mythri <mythridantu1010@gmail.com>
2025-03-10 10:56:44 +0530

    SVS-11: integrated backend with frontend

--- models/User.js
+++ models/User.js
@@ -4,8 +4,9 @@ const UserSchema = new mongoose.Schema({
   Username: { type: String, required: true, unique: true },
   Name: { type: String, required: true },
   Password: { type: String, required: true },
-  Role: { type: String, required: true },
+  Role: { type: String, required: true, enum: ["Student", "Teacher"] },
   Branch: { type: String, required: true },
 });
+
 
 module.exports = mongoose.model("User", UserSchema, "student_users");
